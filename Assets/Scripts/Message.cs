e46cc29538284f8bbba5983e0ee1a4727374f3dd
yoshioka440 <yoshioka440@gmail.com>
2015-01-25 12:14:43 +0900

    メッセージ（Eazy~Hard）の実装

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Message : MonoBehaviour {
+
+	public GameObject parentObj;
+	public GameObject message_eazy;
+	public GameObject message_medium;
+	public GameObject message_hard;
+	
+	public GameObject message_milk1;
+	public GameObject message_milk2;
+	public GameObject message_pizza1;
+	public GameObject message_pizza2;
+	public GameObject message_Drugstore1;
+	public GameObject message_Drugstore2;
+	
+	// Use this for initialization
+	void Start () {
+			
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void AppearMessage() {
+		Instantiate(message_eazy);
+		message_eazy.transform.parent = parentObj.transform;
+	}
+}
