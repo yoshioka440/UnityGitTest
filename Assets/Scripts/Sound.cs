f5212c323ed137e2ec6e8c63343fa696bb35a1be
yoshioka440 <yoshioka440@gmail.com>
2015-01-25 15:38:10 +0900

    SoundManagerの追加

---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//SoundManager.Instance.PlaySE(1);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//SoundManager.Instance.PlaySE(1);
+		if (Input.GetMouseButtonDown (1)) {
+			PlaySE();
+		}
+	}
+	
+	public void PlaySE() {
+		SoundManager.Instance.PlaySE(1);
+		
+	}
+}
