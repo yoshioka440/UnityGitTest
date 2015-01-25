0ca87f0a69982bec9ed8187f4461f5879aecd823
yoshioka440 <yoshioka440@gmail.com>
2015-01-25 11:05:00 +0900

    吹き出しの追加

---
+++ Assets/Scripts/Dice.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dice : MonoBehaviour {
+
+	private GameObject go;
+	private bool flg;
+	
+	private int diceNum;
+	private bool rolled;
+	private bool pushDiceImage;
+	
+	public int DiceNum {
+		//set{this.diceNum = value;}
+		get{return this.diceNum;}
+	}
+	
+	public bool Rooled {
+		//set{this.rolled = value;}
+		get{return rolled;}
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+		go = GameObject.Find("Dice");
+		flg = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		TouchAction();
+		if (GameManager.canRollDice && !flg) {
+			diceNum = RollDice();
+			rolled = true;
+		}
+		
+	}
+	
+	private int RollDice() {
+		// 保留
+		//Random rand = new Random();
+		//return rand.Next(1, 3);
+		
+		return 1;
+	}
+	
+	private void TouchAction ()
+	{
+		// Input.GetMouseButtonDown(0)でマウスクリック取得
+		if (Input.GetMouseButtonDown (0)) {
+			Debug.Log ("LeftClick!");
+			Ray ray = new Ray();
+			RaycastHit hit = new RaycastHit();
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			//マウスクリックした場所からRay？を飛ばし、オブジェクトがあればtrue 
+			if(Physics.Raycast(ray.origin,ray.direction,out hit,Mathf.Infinity)){
+				Debug.Log(hit.collider.gameObject.name);
+				
+				Color color = new Color(Random.value, Random.value, Random.value, 0.1f);
+				hit.collider.renderer.material.color = color;
+				 
+				go.gameObject.SetActive(flg);
+				flg = !flg;
+				
+			}
+			
+		}
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	enum State : int {Dice=1, ShowPlace, ChoosePlace, Task, Item, Finish};
+	private int state;
+	private int diceNum;
+	public int defaulDicetNum = 1;
+	private bool diceRolled;
+	
+	public static bool canRollDice;
+	Dice dice;
+		
+	// Use this for initialization
+	void Start () {
+		state = (int)State.Dice;
+		diceNum = defaulDicetNum;
+		diceRolled = false;
+		Debug.Log ("state: " + state);
+		canRollDice = true;
+		dice = GameObject.Find("Dice").GetComponent<Dice>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+		
+		switch (state) {
+		case 1:
+			//Debug.Log ("state: " + state);
+			//diceRolled = true;
+			
+			if (dice.Rooled) {
+				canRollDice = false;
+				diceNum = dice.DiceNum;
+				state++;
+			}
+			break;
+			
+		case 2:
+			Debug.Log ("state: " + state);
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		case 6:
+			canRollDice = true;
+			break;
+		default :
+			break;
+		}
+	}
+}
