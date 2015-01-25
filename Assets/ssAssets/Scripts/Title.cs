a15fe9f6599e232a59ef3d3439003396066e6a49
GGJ <GGJ.ggj>
2015-01-25 16:50:09 +0900

    Add credit and reset
    add credit
    Fix reset

--- Assets/ssAssets/Scripts/Title.cs
+++ Assets/ssAssets/Scripts/Title.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Title : MonoBehaviour {
 
 
+    public GameObject[] actives;
+    public GameObject[] cards;
 
 	void Start () {
 	
@@ -15,6 +17,9 @@ public class Title : MonoBehaviour {
 
     public void LetsPlay(int num) {
         GameServer.instance.player = num;
+
+        foreach (var a in actives) a.SetActive(true);
+        foreach (var a in cards) a.GetComponent<Card>().disabled = true;
 
         GetComponent<NavButton>().Goto();
     }
